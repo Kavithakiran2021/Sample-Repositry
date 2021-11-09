bb76addf02258742c3d3e87844d689bbbf9eb45e
Kavithakiran2021 <kavithakiran1106@gmail.com>
2021-11-09 12:26:45 +0530

    username functionality

--- src/git/sample/Class1.java
+++ src/git/sample/Class1.java
@@ -2,4 +2,14 @@ package git.sample;
 
 public class Class1 {
 
+	
+	public static void usernameFunctionality() {
+System.out.println("kiran");
+	}
+public static void main(String[] args) {
+	
+	
+	usernameFunctionality();
 }
+	}
+
